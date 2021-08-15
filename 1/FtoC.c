c8c857589d3724980e5c2424db2ce1c2f1585029
TAMAL BAG <87414354+tamalbag117@users.noreply.github.com>
2021-08-15 08:34:07 +0530

    Update FtoC.c

--- 1/FtoC.c
+++ 1/FtoC.c
@@ -1,10 +1,15 @@
-#include<stdio.h>  
+/*farenheit to celcius temperature convertion using function
+Timecomplexity : O(1); */
+#include<stdio.h> 
+float Func(float s){
+    float z=((s-32)*5)/9;
+    return z;
+}
 int main()   
     { 
         float f, c;  
         printf("\nEnter the Temparature : ");
-        scanf("%f",&f);  
-        c=((f-32)*5)/9;  
-        printf("\nTemperature in Celsius is : %.2f",c);  
+        scanf("%f",&f);   
+        printf("\nTemperature in Celsius is : %.2f",Func(f));  
         return 0;  
     }  
